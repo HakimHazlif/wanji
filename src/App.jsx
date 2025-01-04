d3464f226bfdf5eefebaac87c35a0b41d860f3b3
Hakim-56 <hakimhazlif@gmail.com>
2025-01-05 00:42:37 +0100

    Set up the session context

--- src/App.jsx
+++ src/App.jsx
@@ -10,31 +10,34 @@ import Favorites from "./pages/Favorites";
 import Settings from "./pages/Settings";
 import ResetPassword from "./pages/ResetPassword";
 import PageNotFound from "./pages/PageNotFound";
+import SessionContextProvider from "./context/UserContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Home />} />
-          <Route path="movie/:id" element={<Show isMovie={true} />} />
-          <Route path="serie/:id" element={<Show isMovie={false} />} />
-          <Route path={`:user`}>
-            <Route index element={<Profile />} />
-            <Route path="watchlist" element={<Watchlist />} />
-            <Route path="lists" element={<Lists />} />
-            <Route path="favorites" element={<Favorites />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="reset-password" element={<ResetPassword />} />
+    <SessionContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="movie/:id" element={<Show isMovie={true} />} />
+            <Route path="serie/:id" element={<Show isMovie={false} />} />
+            <Route path={`:user`}>
+              <Route index element={<Profile />} />
+              <Route path="watchlist" element={<Watchlist />} />
+              <Route path="lists" element={<Lists />} />
+              <Route path="favorites" element={<Favorites />} />
+              <Route path="settings" element={<Settings />} />
+              <Route path="reset-password" element={<ResetPassword />} />
+            </Route>
           </Route>
-        </Route>
 
-        <Route path="login" element={<SignLog />} />
-        <Route path="signup" element={<SignLog />} />
+          <Route path="login" element={<SignLog />} />
+          <Route path="signup" element={<SignLog />} />
 
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </SessionContextProvider>
   );
 }
 
---
+++ src/context/UserContext.jsx
@@ -0,0 +1,55 @@
+import { createContext, useContext, useState } from "react";
+
+const SessionContext = createContext();
+
+const SessionContextProvider = ({ children }) => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const [openProfileMenu, setOpenProfileMenu] = useState(false);
+
+  const [user, setUser] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  function toggleLogsForm(button) {
+    setIsLogin(button === "login" ? true : false);
+  }
+
+  function toggleProfileMenu() {
+    setOpenProfileMenu((prev) => !prev);
+  }
+
+  function handleChangeUser(e, label) {
+    const { value } = e.target;
+
+    setUser({ ...user, [label]: value });
+  }
+
+  return (
+    <SessionContext.Provider
+      value={{
+        isLogin,
+        openProfileMenu,
+        user,
+        toggleLogsForm,
+        toggleProfileMenu,
+        handleChangeUser,
+      }}
+    >
+      {children}
+    </SessionContext.Provider>
+  );
+};
+
+export function useSession() {
+  const context = useContext(SessionContext);
+
+  if (!context)
+    throw new Error("DarkModeContext was used outside of DatkModeProvider");
+
+  return context;
+}
+
+export default SessionContextProvider;
