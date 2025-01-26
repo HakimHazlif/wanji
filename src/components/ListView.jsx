8ef9f705e42310c36669c5844b4c67ae29b43b13
Hakim-56 <hakimhazlif@gmail.com>
2025-01-26 04:40:21 +0100

    Add a style to empty list

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -6,6 +6,8 @@ import {
 } from "../features/userLists/useFetchItemsList ";
 import Spinner from "../ui/Spinner";
 import SpinnerMini from "../ui/SpinnerMini";
+import { RiMovie2AiFill, RiMovie2AiLine } from "react-icons/ri";
+import { BiMoviePlay, BiSolidMoviePlay } from "react-icons/bi";
 
 const ListView = ({ listId }) => {
   const { lists } = useLists();
@@ -32,7 +34,30 @@ const ListView = ({ listId }) => {
     }
   };
 
-  if (!listId || !itemsList || !itemsList.items.length) return <p>Empty</p>;
+  if (!listId || !itemsList || !itemsList.items.length) {
+    return (
+      <section className="mt-5 flex flex-col items-center justify-center h-screen text-center bg-gradient-to-b from-gray-900 to-gray-800 text-white">
+        <div className="flex flex-col items-center gap-6">
+          <div className="bg-gray-700 p-6 rounded-full shadow-lg">
+            <RiMovie2AiLine className="h-20 w-20 text-purple-600" />
+          </div>
+
+          <h2 className="text-2xl font-bold ">No Movies or Shows Found</h2>
+          <p className="text-gray-400 max-w-md">
+            We couldn&apos;t find any content in your list. Start exploring new
+            movies, TV shows, or episodes to fill it up. 🎥🍿
+          </p>
+
+          <button
+            onClick={() => console.log("Redirect to explore page")}
+            className="px-6 py-3 bg-orange-amber rounded-lg shadow-md hover:bg-orange-coral transition text-gray-800 font-semibold"
+          >
+            Explore Movies & Shows
+          </button>
+        </div>
+      </section>
+    );
+  }
 
   if (isLoading) return <Spinner />;
 
--- src/ui/CreateListButton.jsx
+++ src/ui/CreateListButton.jsx
@@ -1,7 +1,12 @@
+import { FaPlus } from "react-icons/fa";
+
 const CreateListButton = () => {
   return (
-    <button className="w-60 py-3 bg-orange-amber rounded-full font-bold text-gray-900">
-      Create a new list
+    <button className="w-60 py-3 bg-orange-amber rounded-full flex justify-center items-center hover:bg-orange-coral transition-colors duration-300">
+      <div className="flex items-center gap-3 font-bold text-gray-900">
+        <FaPlus />
+        <span>Create a new list</span>
+      </div>
     </button>
   );
 };
