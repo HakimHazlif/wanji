8e5924923565a3b6ca5443a9d0c81450a6298d1c
Hakim-56 <hakimhazlif@gmail.com>
2025-05-06 10:59:04 +0100

    Refactor: Update max length constants for name, description, username, and bio

--- src/constants/variables.js
+++ src/constants/variables.js
@@ -1,6 +1,6 @@
-export const MAX_NAME_LENGTH = 70;
-export const MAX_DESCRIPTION_LENGTH = 1000;
+export const MAX_NAME_LENGTH = 50;
+export const MAX_DESCRIPTION_LENGTH = 500;
 export const MAX_USERNAME_LENGTH = 40;
-export const MAX_BIO_LENGTH = 800;
+export const MAX_BIO_LENGTH = 400;
 
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
