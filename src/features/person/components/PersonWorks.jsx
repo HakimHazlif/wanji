4cb1fa2004437e3080d07c8062c50c81789c50d0
Hakim-56 <hakimhazlif@gmail.com>
2025-05-14 13:42:25 +0100

    refactor: use useMemo instead of useEffect for filtering and sorting for prseon works. Also, use useItemsStatus for person works to track watchlist, favorite, rating, and user lists states

--- src/features/person/components/PersonWorks.jsx
+++ src/features/person/components/PersonWorks.jsx
@@ -1,17 +1,19 @@
-import { useEffect } from "react";
+import { useMemo } from "react";
 import { usePerson } from "../hooks/usePerson";
 import WorkCard from "./WorkCard";
 import EmptyDepartment from "../../../ui/EmptyDepartment";
 import WorksControlBar from "./WorksControlBar";
 import { usePersonWorksContext } from "../../../context/PersonWorksContext";
+import Spinner from "../../../ui/Spinner";
 
 const PersonWorks = () => {
-  const { activeTab, departmentFilter, sortBy, setWorks, works } =
-    usePersonWorksContext();
+  const { activeTab, departmentFilter, sortBy } = usePersonWorksContext();
 
-  const { personMovies, personTv, personDetails } = usePerson();
+  const { personMovies, personTv, personDetails, isLoading } = usePerson();
 
-  useEffect(() => {
+  console.log({ personMovies, personTv });
+
+  const filteredWorks = useMemo(() => {
     const castMovies = personMovies?.cast || [];
     const crewMovies = personMovies?.crew || [];
     const castTvShows = personTv?.cast || [];
@@ -32,64 +34,64 @@ const PersonWorks = () => {
       works = crewWorks?.filter((work) => work.department === departmentFilter);
     }
 
-    const sortWorks = (works) => {
-      return works?.sort((a, b) => {
-        switch (sortBy) {
-          case "date-latest":
-            return (
-              new Date(b?.release_date || b?.first_air_date) -
-              new Date(a?.release_date || a?.first_air_date)
-            );
-          case "date-oldest":
-            return (
-              new Date(a?.release_date || a?.first_air_date) -
-              new Date(b?.release_date || b?.first_air_date)
-            );
-          case "Popularity-high":
-            return b?.popularity - a?.popularity;
-          case "Popularity-low":
-            return a?.popularity - b?.popularity;
-          default:
-            return (
-              new Date(b?.release_date || b?.first_air_date) -
-              new Date(a?.release_date || a?.first_air_date)
-            );
-        }
-      });
-    };
-
-    const newWorks = sortWorks(works);
-
-    setWorks(newWorks);
+    return works;
   }, [
-    activeTab,
-    departmentFilter,
-    sortBy,
     personMovies?.cast,
     personMovies?.crew,
     personTv?.cast,
     personTv?.crew,
-    setWorks,
+    activeTab,
+    departmentFilter,
   ]);
+
+  const sortedWorks = useMemo(() => {
+    return filteredWorks?.sort((a, b) => {
+      switch (sortBy) {
+        case "date-latest":
+          return (
+            new Date(b?.release_date || b?.first_air_date) -
+            new Date(a?.release_date || a?.first_air_date)
+          );
+        case "date-oldest":
+          return (
+            new Date(a?.release_date || a?.first_air_date) -
+            new Date(b?.release_date || b?.first_air_date)
+          );
+        case "Popularity-high":
+          return b?.popularity - a?.popularity;
+        case "Popularity-low":
+          return a?.popularity - b?.popularity;
+        default:
+          return (
+            new Date(b?.release_date || b?.first_air_date) -
+            new Date(a?.release_date || a?.first_air_date)
+          );
+      }
+    });
+  }, [filteredWorks, sortBy]);
 
   return (
     <section className="space-y-6 ">
       <WorksControlBar />
-      <section className="pt-10">
-        {works?.length > 0 ? (
-          <div className="grid grid-flow-row gap-8">
-            {works.map((work) => (
-              <WorkCard
-                key={work.credit_id}
-                show={work}
-                category={work["title"] ? "movie" : "tv"}
-              />
-            ))}
-          </div>
-        ) : (
-          <EmptyDepartment name={personDetails?.name} />
-        )}
-      </section>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <section className="pt-10">
+          {sortedWorks?.length > 0 ? (
+            <div className="grid grid-flow-row gap-8">
+              {sortedWorks.map((work) => (
+                <WorkCard
+                  key={work.credit_id}
+                  show={work}
+                  category={work["title"] ? "movie" : "tv"}
+                />
+              ))}
+            </div>
+          ) : (
+            <EmptyDepartment name={personDetails?.name} />
+          )}
+        </section>
+      )}
     </section>
   );
 };
--- src/features/person/hooks/usePerson.js
+++ src/features/person/hooks/usePerson.js
@@ -1,11 +1,13 @@
 import { useQuery } from "react-query";
 import { useParams } from "react-router";
 import { getPersonData } from "../api/personApi";
+import { useItemsStatus } from "../../userLists/hooks/useItemsStatus";
+import { useMemo } from "react";
 
 export function usePerson() {
   const { personId } = useParams();
 
-  const { isLoading, data: personData } = useQuery({
+  const { isLoading: isPersonLoading, data: personData } = useQuery({
     queryKey: ["person", personId],
     queryFn: () => getPersonData(personId),
     onError: (err) => {
@@ -18,11 +20,34 @@ export function usePerson() {
   const personTv = personData?.tvCredits;
   const personImages = personData?.images;
 
+  const itemIds = useMemo(() => {
+    const castMovies = personMovies?.cast ?? [];
+    const crewMovies = personMovies?.crew ?? [];
+    const castTv = personTv?.cast ?? [];
+    const crewTv = personTv?.crew ?? [];
+
+    const moviesIds = [
+      ...new Set([...castMovies, ...crewMovies].map((movie) => movie.id)),
+    ];
+    const tvIds = [...new Set([...castTv, ...crewTv].map((tv) => tv.id))];
+
+    return {
+      moviesIds,
+      tvIds,
+    };
+  }, [personMovies?.cast, personMovies?.crew, personTv?.cast, personTv?.crew]);
+
+  const { isLoading: isMovieStatusLoading } = useItemsStatus(
+    itemIds.moviesIds,
+    "movie"
+  );
+  const { isLoading: isTvStatusLoading } = useItemsStatus(itemIds.tvIds, "tv");
+
   return {
     personDetails,
     personImages,
     personMovies,
     personTv,
-    isLoading,
+    isLoading: isPersonLoading || isMovieStatusLoading || isTvStatusLoading,
   };
 }
