4f0e1acf30ce69456fb077747afb20874fc00ddb
Hakim-56 <hakimhazlif@gmail.com>
2025-06-23 05:31:03 +0100

    style: fix responsivity of MediaSectionHeader

--- src/components/MediaSectionHeader.jsx
+++ src/components/MediaSectionHeader.jsx
@@ -2,7 +2,7 @@ import MediaFilterTabs from "./MediaFilterTabs";
 
 const MediaSectionHeader = ({ title, description, typeTag = "" }) => {
   return (
-    <div className="border-b border-slate-700 pb-5 w-full flex items-end justify-between gap-10">
+    <div className="border-b border-slate-700 pb-5 w-full flex md:flex-row flex-col items-end justify-between md:gap-10 gap-5">
       <div>
         <h1 className="text-4xl font-bold">{title}</h1>
         <p className="mt-5 text-base text-slate-400">{description}</p>
